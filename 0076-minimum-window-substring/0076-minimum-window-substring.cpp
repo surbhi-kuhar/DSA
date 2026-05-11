eca2dfaaf457baaa6a4f68ce71e25a2266f0e1d1
Surbhi Kuhar <104093804+surbhi-kuhar@users.noreply.github.com>
2026-05-11 14:24:17 +0530

    Time: 17 ms (25.58%), Space: 11.7 MB (23.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,48 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        unordered_map<char,int>freqt;
+        unordered_map<char,int>currWin;
 
-        unordered_map<char,int> freqt;
-        unordered_map<char,int> currWin;
+        for(auto x:t) freqt[x]++;
 
-        for(auto x : t) freqt[x]++;
-
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
 
         int matched = 0;
-
+        int ans = INT_MAX;
+        string str = "";
         int start = 0;
-        int minLen = INT_MAX;
 
-        while(j < s.size()) {
+        while(j<s.size()){
 
             currWin[s[j]]++;
-
-            if(freqt.count(s[j]) &&
-               currWin[s[j]] == freqt[s[j]]) {
-                matched++;
-            }
-
-            while(matched == freqt.size()) {
-
-                if(j - i + 1 < minLen) {
-                    minLen = j - i + 1;
+            if(freqt.count(s[j]) && freqt[s[j]]==currWin[s[j]]) matched++;
+            
+            while(matched == freqt.size()){
+                if(j-i+1 < ans){
+                    ans = min(ans,j-i+1);
                     start = i;
                 }
-
                 currWin[s[i]]--;
-
-                if(freqt.count(s[i]) &&
-                   currWin[s[i]] < freqt[s[i]]) {
-                    matched--;
-                }
-
+                if(freqt.count(s[i]) && currWin[s[i]]<freqt[s[i]]) matched--;
                 i++;
             }
 
             j++;
         }
 
-        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+        return ans==INT_MAX ? "" : s.substr(start,ans);
     }
 };
